8e171c55cf8b23327db27fb37f614738b053bbf8
U-DDA01\csd80117 <ktye@users.noreply.github.com>
2019-10-23 16:52:29 +0200

    grey to bitmap and reshape

--- README.md
+++ README.md
@@ -3,12 +3,21 @@
 ced #RRGGBBAA #RRGGBBAA ... in.png
 replaces colors in png file and writes out.png
 
-e.g. show colors in in.png
+(1) single argument: show colors in in.png
 $ ced in.png
 #000000ff 3090
 #ff0000ff 7851
 #00000000 14318
 
-# replace transparent black with white
-$ ced #00000000 #FFFFFFFF in.png
+
+(2) two arguments: create black-white from greyscale with threshold
+$ ced aa in.png
+
+(3) replace color pairs
+# e.g. replace transparent black with white
+$ ced '#00000000' '#FFFFFFFF' in.png
+
+(4) catenate images in rows/cols
+# append all images in two rows and three columns
+$ ced 2 '3#' *.png
 ```
--- main.go
+++ main.go
@@ -4,44 +4,71 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"image/draw"
 	"image/png"
 	"os"
 	"sort"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	a := os.Args[1:]
+	grey := uint8(0)
+	var colors []color.RGBA
 	if len(a) == 1 {
 		histogram(a[0])
 		return
-	}
-	if len(a) < 3 || len(a)%2 == 0 {
+	} else if len(a) == 2 {
+		grey = parseGrey(a[0])
+		if grey == 0 {
+			panic("grey must be > 0")
+		}
+	} else if len(a) > 2 && strings.HasSuffix(a[1], "#") {
+		reshape(a)
+		return
+	} else if len(a) < 3 || len(a)%2 == 0 {
 		panic("not enough arguments")
+	} else {
+		a = a[:len(a)-1]
+		colors = make([]color.RGBA, len(a))
+		for i := range a {
+			colors[i] = parseColor(a[i])
+		}
 	}
 	m, w, h := readImage(a[len(a)-1])
-	a = a[:len(a)-1]
-	colors := make([]color.RGBA, len(a))
-	for i := range a {
-		colors[i] = parseColor(a[i])
-	}
 	d := image.NewRGBA(image.Rectangle{Max: image.Point{w, h}})
+	var c color.Color
 	for i := 0; i < w; i++ {
 		for j := 0; j < h; j++ {
-			c := color.RGBAModel.Convert(m.At(i, j)).(color.RGBA)
-			for k := 0; k < len(colors); k += 2 {
-				if colors[k] == c {
-					c = colors[k+1]
-					break
+			if grey == 0 {
+				c = color.RGBAModel.Convert(m.At(i, j)).(color.RGBA)
+				for k := 0; k < len(colors); k += 2 {
+					if colors[k] == c {
+						c = colors[k+1]
+						break
+					}
+				}
+			} else {
+				rgba := color.RGBAModel.Convert(m.At(i, j)).(color.RGBA)
+				if rgba.R <= grey {
+					c = color.Black
+				} else {
+					c = color.White
 				}
 			}
 			d.Set(i, j, c)
 		}
 	}
-	out, e := os.Create("out.png")
+	writeImage(d)
+}
+func parseGrey(s string) uint8 { // XX
+	if len(s) != 2 {
+		panic("parse grey")
+	}
+	u, e := strconv.ParseUint(s, 16, 32)
 	fail(e)
-	defer out.Close()
-	fail(png.Encode(out, d))
+	return uint8(u)
 }
 func parseColor(s string) color.RGBA { // #RRGGBB #RRGGBBAA
 	if len(s) == 7 {
@@ -60,6 +87,12 @@ func readImage(file string) (image.Image, int, int) {
 	m, e := png.Decode(f)
 	fail(e)
 	return m, m.Bounds().Dx(), m.Bounds().Dy()
+}
+func writeImage(m image.Image) {
+	out, e := os.Create("out.png")
+	fail(e)
+	defer out.Close()
+	fail(png.Encode(out, m))
 }
 func histogram(file string) {
 	m, w, h := readImage(file)
@@ -84,6 +117,42 @@ func histogram(file string) {
 		c := colors[i]
 		fmt.Printf("#%02x%02x%02x%02x %d\n", c.R, c.G, c.B, c.A, colormap[c])
 	}
+}
+func reshape(a []string) {
+	var rows, cols int
+	var e error
+	rows, e = strconv.Atoi(a[0])
+	fail(e)
+	cols, e = strconv.Atoi(strings.TrimSuffix(a[1], "#"))
+	fail(e)
+	a = a[2:]
+	d := image.NewRGBA(image.Rectangle{})
+	n := 0
+	for i := 0; i < rows; i++ {
+		dr := image.NewRGBA(image.Rectangle{})
+		for j := 0; j < cols; j++ {
+			m, _, _ := readImage(a[n%len(a)])
+			dr = cat(dr, m, true)
+			n++
+		}
+		d = cat(d, dr, false)
+	}
+	writeImage(d)
+}
+func cat(x, y image.Image, hor bool) *image.RGBA {
+	var r image.Rectangle
+	r.Max = image.Point{y.Bounds().Dx(), x.Bounds().Dy() + y.Bounds().Dy()}
+	if hor {
+		r.Max = image.Point{x.Bounds().Dx() + y.Bounds().Dx(), y.Bounds().Dy()}
+	}
+	m := image.NewRGBA(r)
+	draw.Draw(m, x.Bounds(), x, image.ZP, draw.Src)
+	r = y.Bounds().Add(image.Point{0, x.Bounds().Dy()})
+	if hor {
+		r = y.Bounds().Add(image.Point{x.Bounds().Dx(), 0})
+	}
+	draw.Draw(m, r, y, image.ZP, draw.Src)
+	return m
 }
 func fail(e error) {
 	if e != nil {
